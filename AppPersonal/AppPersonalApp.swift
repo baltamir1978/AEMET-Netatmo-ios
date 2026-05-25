90715db72f5ad885131b4ffe68894b7809905fca
Bruno Altamirano <brunoaltfercorgmail.com>
2026-05-25 13:02:31 +0200

    Initial Commit

---
+++ AppPersonal/AppPersonalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPersonalApp.swift
+//  AppPersonal
+//
+//  Created by Bruno Altamirano on 25/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct AppPersonalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
